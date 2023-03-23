04be69e49a9ab5979a3811d0acc2db7c5e541cd0
jin <schoolyoung2m@naver.com>
2023-03-23 20:06:45 +0900

    모바일 반응형 수정

--- html/js/qna_tab.js
+++ html/js/qna_tab.js
@@ -8,12 +8,26 @@ t_h_line.forEach((item,index)=>{
     item.addEventListener("click",(e)=>{
         //페이지 이동방지
         e.preventDefault();
-        //활성화 전 전부 비활성화
-        t_text_box.forEach((item,index)=>{
-            item.style.height = 0;
-            // allow[index].innerHTML = `<i class="fa-solid fa-angle-down"></i>`
-        });
-        t_text_box[index].style.height = "202px";
-        // allow[index].innerHTML = `<i class="fa-solid fa-angle-up"></i>`.innerHTML = `<i class="fa-solid fa-angle-up"></i>`
+
+        //브라우저 가로크기값 가져오기
+        let view_width = document.body.offsetWidth;
+
+        //모바일로 볼 경우
+        if(view_width <= 430){
+            tab_clear(375);
+            t_text_box[index].style.height = "375px";
+        }
+        //그 외
+        else {
+            tab_clear(202);
+            t_text_box[index].style.height = "202px";
+        }
     });
 });
+
+function tab_clear(int){
+    //활성화 전 전부 비활성화
+    t_text_box.forEach((item,index)=>{
+        item.style.height = 0;
+    });
+}
